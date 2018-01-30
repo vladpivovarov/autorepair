0148fdef24bb1ffe8ba5cd839156b3bcf3ce4591
Vladislav <vladpivovarov2797@ya.ru>
2018-01-31 00:08:34 +0300

    ready

--- build/mail2.php
+++ build/mail2.php
@@ -1,12 +1,18 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>Заказ на деталь</title>
 </head>
 
 <body>
+
 <?php
+// Вывод заголовка с данными о кодировке страницы
+header('Content-Type: text/html; charset=utf-8');
+// Настройка локали
+setlocale(LC_ALL, 'ru_RU.65001', 'rus_RUS.65001', 'Russian_Russia. 65001', 'russian');
+
 function send_mail()
 {
     $name = htmlspecialchars($_REQUEST['name']);
@@ -14,14 +20,14 @@ function send_mail()
 {
     $phone = htmlspecialchars($_REQUEST['phone']);
 }
-$message = '<b>Имя: </b>'.$_REQUEST['name'].'<br> <b>Телефон: </b>'.$_REQUEST['phone'].'';
+$message = iconv('UTF-8', 'cp1251', "<b>Заказ на деталь</b><br><b>Имя: </b> ").iconv('UTF-8', 'cp1251', $_REQUEST['name']).iconv('UTF-8', 'cp1251', "<br> <b>Телефон: </b>").iconv('UTF-8', 'cp1251', $_REQUEST['phone']).'';
 
 include "class.phpmailer.php";// подключаем класс
 
 $mail = new PHPMailer();
-$mail->From = $_REQUEST['phone'];
-$mail->FromName = $_REQUEST['name'];
-$mail->AddAddress('texnik.avtoservis@mail.ru');
+$mail->From = iconv('UTF-8', 'cp1251', $_REQUEST['phone']);
+$mail->FromName = iconv('UTF-8', 'cp1251', $_REQUEST['name']);
+$mail->AddAddress('vladpivovarov2797@ya.ru');
 $mail->IsHTML(true);
 $mail->Subject = $_POST['title'];
 
@@ -33,6 +39,8 @@ if(isset($_FILES['files']))
     }
 }
 $mail->Body = $message;
+
+
 if (!$mail->Send()) die ('Mailer Error: '.$mail->ErrorInfo);
 {
     echo '<center><b>Спасибо за Ваш заказ! В ближайшее время мы свяжемся с Вами.<br><a href=index.html>Нажмите</a>, чтобы вернуться на главную страницу';
